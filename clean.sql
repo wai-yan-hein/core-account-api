17d44b65643cb8467d4230d0b22201999fb24cf9
WaiYanHain <waiyanhain215@gmail.com>
2023-01-02 17:42:22 +0630

    w

---
+++ clean.sql
@@ -0,0 +1,10 @@
+truncate department;
+truncate gl;
+truncate gl_log;
+truncate seq_table;
+truncate stock_op_value;
+truncate stock_op_value;
+truncate tmp_closing;
+truncate tmp_dep_filter;
+truncate tmp_tri;
+truncate trader;
